06b448df936a35d4ff8fcca7d17b89d8ddf5b498
anningwang <wxg001@21cn.com>
2017-07-31 19:41:48 +0800

    增加 会议室，总裁办公室1，仓库、前台等文字标识。

--- run.py
+++ run.py
@@ -1,13 +1,7 @@
 #!flask/bin/python
 # -*- coding:utf-8 -*-
-from app import app, lbs
-from app import dijkstra
+from app import app
 
-
-print dijkstra.min_dist2(5, 20)
-print dijkstra.min_dist2(5, 21)
-print dijkstra.min_dist2(20, 5)
 # app.run(debug=True)
-
 if __name__ == '__main__':
     app.run(host='192.168.31.181', port=80, debug=True, use_reloader=False, threaded=True)
